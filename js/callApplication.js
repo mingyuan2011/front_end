fa6e844a478e0e6564ed2a8668833da1fd38fedb
mingyuan2011 <lzc.sdu@gmail.com>
2014-10-26 18:40:22 +0800

    adding code for call

---
+++ js/callApplication.js
@@ -0,0 +1,15 @@
+function Person(name, age){
+	this.name = name;
+	this.age  = age;
+}
+
+function Student(name, age, grade){
+	//Person.call(this, arguments);
+	Person.call(this, name, age);
+	this.grade = grade;
+}
+
+var student = new Student("Jim", 18, "Freshman");
+console.log("Name: " + student.name);
+console.log("Age : " + student.age);
+console.log("Grade: " + student.grade);
