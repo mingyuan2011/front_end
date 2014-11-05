afe8229dd7a353ceeb9090da31c64031fa71b804
mingyuan2011 <lzc.sdu@gmail.com>
2014-11-05 13:58:01 +0800

    Adding some code for my jQuery

---
+++ myjQuery/myjQuery.js
@@ -0,0 +1,24 @@
+/*
+    This jQuery Source code is a copy of jQuery source code for learning purpose.
+    If it offends your copyright, plz contact me via lzc.sdu#gmail.com to delete this 
+    repository.
+*/
+
+
+(function(){
+    
+    var _jQuery = window.jQuery;
+    var _$ = window.$;
+    
+    var jQuery = window.$ = window.jQuery = function(selector, context){
+        /// TODO
+        return new jQuery.fn.init(selector, context);
+    };
+    
+    jQuery.prototype = {
+        init: function(selector, context){
+            ///TODO
+        }
+    };
+    
+})();
